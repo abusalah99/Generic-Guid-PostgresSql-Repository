06a73732d9bb7528eeacbd43ed9b49227b7ba985
abu_salah <abu_salah@yahoo.com>
2024-12-24 02:43:38 +0200

    commit

--- GenericGuidPostgresSqlRepository/DependencyInjection.cs
+++ GenericGuidPostgresSqlRepository/DependencyInjection.cs
@@ -2,9 +2,15 @@ namespace GenericGuidPostgresSqlRepository;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddRepository(this IServiceCollection services)
+    public static IServiceCollection AddRepository<TContext>(this IServiceCollection services)
+        where TContext : DbContext
     {
-        services.AddScoped(typeof(IRepository<>), typeof(Repository<,>));
+        services.AddScoped(typeof(IRepository<>), provider =>
+        {
+            var context = provider.GetRequiredService<TContext>();
+            var repositoryType = typeof(Repository<,>).MakeGenericType(typeof(BaseEntity), typeof(TContext));
+            return Activator.CreateInstance(repositoryType, context)!;
+        });
 
         return services;
     }
